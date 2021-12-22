9960a86b04fbfb3a985d2027849a0af8628474b3
unknown <you@example.com>
2021-12-22 08:51:15 +0530

    Start collection class TDD

---
+++ tests/unit/CollectionClassTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CollectionClassTest extends TestCase
+{
+    public function testTrueAssertToTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
